cfca904dbece0bbe3db318fcdb124c0cbc1fd7a8
ynnako <ynnako.junk@gmail.com>
2019-12-27 10:08:04 +0200

    implemented some methods

--- src/homework2/BipartiteGraph.java
+++ src/homework2/BipartiteGraph.java
@@ -71,7 +71,7 @@ public class BipartiteGraph<T> {
     *@effects returns an ArrayList of type T with all the elements of black nodes from the graph. holds the reference to the objects themselves
     *@returns an array list with references to the black nodes in graph.
      */
-    public <T> ArrayList<T> listBlackNodes(){return}
+    public ArrayList<T> listBlackNodes(){return}
 
     /*
      *@requires T must be immutable and have an overridden equals method
@@ -97,14 +97,23 @@ public class BipartiteGraph<T> {
      * @effects returns an arraylist of the parents of the child label. if childLabel is null or non existent
      *          throws an exception.
      */
-    public <T> ArrayList<T> listParent(T childLabel) throws NullPointerException {}
+    public ArrayList<T> listParent(T childLabel) throws NullPointerException {
+    }
 
     /*
     * @requires - T must be immutable and override equals method
     * @modifies - nothing.
     * @effects - returns the child label of type T that is connected with edge edgelabel to the parent parentlabel in this
      */
-    public <T> T getChildByEdgeLabel(T parentLabel, T edgeLabel) throws NullPointerException {}
+    public T getChildByEdgeLabel(T parentLabel, T edgeLabel) throws NullPointerException {
+        if(parentLabel == null || edgeLabel == null) throw new NullPointerException();
+        for(Edege<T> i : edges){
+            if(i.getLable().equals(edgeLabel) && i.getParent().getLable().equals(parentLabel)){
+                return i.getChild().getLable();
+            }
+        }
+        return null;
+    }
 
 
     /*
@@ -112,6 +121,14 @@ public class BipartiteGraph<T> {
      * @modifies - nothing.
      * @effects - returns the parent label of type T that is connected with edge edgelabel to the child childlabel in this
      */
-    public <T> T getParentdByEdgeLabel(T parentLabel, T edgeLabel) throws NullPointerException {}
+    public T getParentdByEdgeLabel(T childLable, T edgeLabel) throws NullPointerException {
+        if(childLable == null || edgeLabel == null) throw new NullPointerException();
+        for(Edege<T> i : edges){
+            if(i.getLable().equals(edgeLabel) && i.getChild().getLable().equals(childLable)){
+                return i.getParent().getLable();
+            }
+        }
+        return null;
+    }
 
 }
